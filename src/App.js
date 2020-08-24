fb6522545aaef07191cf3513de061a11ea2b6aa3
Sosuke Brause <brosuke89@gmail.com>
2020-08-24 16:02:29 -0700

    add profile component and basic styling to components for visibility; installed react-json-pretty to import JSONPretty in Profile.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Profile />
       <LoginButton />
       <LogoutButton />
     </>
