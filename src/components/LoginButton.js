25d352788ea35fb8b6cdf8f3fc587821ba7406bf
Sosuke Brause <brosuke89@gmail.com>
2020-08-24 16:18:22 -0700

    made login/logout buttons only appear when appropriate

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -2,12 +2,14 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
 
   return (
-    <div>
-      <button onClick={() => loginWithRedirect()}>Login</button>
-    </div>
+    !isAuthenticated && (
+      <div>
+        <button onClick={() => loginWithRedirect()}>Login</button>
+      </div>
+    )
   );
 };
 
